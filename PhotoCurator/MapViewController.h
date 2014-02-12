fdcd1ad13650ca0cc24b67f9b534e4b081e0a45c
tokyorefrain <tokyorefrain@tokyorefrain-no-MacBook-Air.local>
2014-02-12 20:44:03 +0900

    1.0.1
    To implement the navigation controller

---
+++ PhotoCurator/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  PhotoCurator
+//
+//  Created by tokyorefrain on 2014/02/12.
+//  Copyright (c) 2014年 tokyorefrain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
--- PhotoCurator/ViewController.m
+++ PhotoCurator/ViewController.m
@@ -18,6 +18,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+
 }
 
 - (void)didReceiveMemoryWarning
